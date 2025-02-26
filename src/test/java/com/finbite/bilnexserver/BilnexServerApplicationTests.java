67aa4fd5ddfad86c08722b4d0340cedcced21d6a
vinodjohn <vinodjohn56@gmail.com>
2025-02-27 01:00:40 +0200

    First commit with some added deps

--- src/test/java/com/finbite/bilnexserver/BilnexServerApplicationTests.java
+++ src/test/java/com/finbite/bilnexserver/BilnexServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BilnexServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
