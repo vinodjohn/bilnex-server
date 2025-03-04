9e32aea60b6ff4ee3413bbcbf129459fca9cc90c
vinodjohn <vinodjohn56@gmail.com>
2025-03-04 05:45:54 +0200

    Added AWS endpoint

--- src/main/java/com/finbite/bilnexserver/auth/configs/SecurityConfiguration.java
+++ src/main/java/com/finbite/bilnexserver/auth/configs/SecurityConfiguration.java
@@ -66,7 +66,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://3.71.42.193"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
